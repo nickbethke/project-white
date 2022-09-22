81ec4bb10af00cee098fe22ad87ba6d9da51b784
nickb <nickbethke5599@gmail.com>
2022-09-23 01:38:20 +0200

    CLI v.1.1c

--- bin/cli.php
+++ bin/cli.php
@@ -5,7 +5,7 @@ define("ABSPATH", dirname(__DIR__) . DIRECTORY_SEPARATOR);
 require_once ABSPATH . "components/abstract/cli/Runnable.php";
 if (version_compare(phpversion(), '8.1.0', '<')) {
     echo CLI\Runnable::header();
-    echo "\t PHP Version to low\n\t Currently running PHP version " . phpversion() . "\n\t Please use 8.1.0 or higher";
+    echo "\t PHP Version to low\n\t Currently running PHP version " . phpversion() . "\n\t Please use 8.1.0 or higher\n\n";
     exit;
 }
 
