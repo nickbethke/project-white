9a39040946d0a4cbdc5e85e9a61a94ab1a9125c7
Nick Bethke <52879960+nickbethke@users.noreply.github.com>
2023-06-05 12:49:22 +0200

    Update database-backup.php

--- config/database-backup.php
+++ config/database-backup.php
@@ -1,6 +1,6 @@
 <?php
 
-const DB_NAME = 'ntkzmu_project_white';
-const DB_USER = 'ntkzmu';
-const DB_PASSWORD = 'gKNDvXpycUuM8m5k';
-const DB_HOST = 'sql289.your-server.de';
+const DB_NAME = '';
+const DB_USER = '';
+const DB_PASSWORD = '';
+const DB_HOST = '';
